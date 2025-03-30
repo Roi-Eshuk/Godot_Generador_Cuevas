f3c436e13d1f51d48d29f00134f81758599ad79e
Roi Eshuk <roi.eshuk@gmail.com>
2025-03-30 13:30:44 -0600

    Regiones de código
    Se agregaron regiones de código

--- generador.gd
+++ generador.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+#region - Variables
+
 #Modo revisión
 @export var modo_revision: bool = true
 var corriendo_revision: bool = false
@@ -24,14 +26,23 @@ var dado: RandomNumberGenerator = RandomNumberGenerator.new()
 @export var usar_semilla: bool = true
 @export var semilla: String = "Roi Eshuk"
 
-func _process(delta: float) -> void:
-	mover_camara(delta)
+#Alisado (cellular automata)
+@export var cantidad_alisado: int = 3
+@export var umbral_ocupar: int = 4
+@export var umbral_vaciar: int = 4
+
+#endregion - Variables
+
+#region - funciones de interacción
 
 func _ready() -> void:
 	if not modo_revision:
 		generar_cueva()
 	else:
 		print("Corriendo en modo de revisión. Presiona ENTER para iniciar.")
+
+func _process(delta: float) -> void:
+	mover_camara(delta)
 
 func mover_camara(delta: float) -> void:
 	var dir_camara = Vector2.ZERO
@@ -45,6 +56,10 @@ func _input(event) -> void:
 		corriendo_revision = true
 		generar_cueva()
 
+#endregion - Funciones de interacción
+
+#region - Funciones del modo revisión
+
 func esperar_teclado(mensaje: String = "Siguiente paso.") -> void:
 	if OS.is_debug_build() and modo_revision and corriendo_revision:
 		dibujar_cueva(cueva)
@@ -53,6 +68,10 @@ func esperar_teclado(mensaje: String = "Siguiente paso.") -> void:
 			await get_tree().process_frame
 			if Input.is_action_just_pressed("ui_siguiente"):
 				break
+
+#endregion - Funciones del modo revisión
+
+#region - Proceso maestro
 
 func generar_cueva() -> void:
 
@@ -67,11 +86,33 @@ func generar_cueva() -> void:
 	await esperar_teclado("¡Cueva terminada! Preseiona ENTER para empezar de nuevo.")
 	corriendo_revision = false
 
+#endregion - Proceso maestro
+
+#region - Funciones de proceso
+
 func reestablecer_variables() -> void:
 	formar_matriz(cueva, ancho, alto, 0)
 	formar_matriz(pivote, ancho, alto, 0)
 	if OS.is_debug_build() and modo_revision:
 		print("Variables reestablecidas.")
+
+func llenar_cueva(matriz: Array = cueva) -> void:
+	
+	tirar_dado()
+	
+	var vaciar: int = 0
+	for x in range(ancho):
+		for y in range(alto):
+			matriz[x][y] = 0
+			if x == 0 or y == 0 or x == ancho - 1 or y == alto - 1:
+				matriz[x][y] = lleno
+			else:
+				vaciar = dado.randi_range(0, 100)
+				matriz[x][y] = vacio if vaciar > densidad else lleno
+
+#endregion - Funciones de proceso
+
+#region - Funciones subordinadas y de apoyo
 
 func formar_matriz(matriz: Array, filas: int = ancho, columnas: int = alto, valor: int = 0) -> Array:
 	matriz.clear()
@@ -88,20 +129,6 @@ func dibujar_cueva(matriz: = cueva, esquina_x: int = 0, esquina_y: int = 0) -> v
 			for y in range(alto):
 				mapa.set_cell(Vector2i(esquina_x + x, esquina_y + y), 0, Vector2i(matriz[x][y], 0))
 
-func llenar_cueva(matriz: Array = cueva) -> void:
-	
-	tirar_dado()
-	
-	var vaciar: int = 0
-	for x in range(ancho):
-		for y in range(alto):
-			matriz[x][y] = 0
-			if x == 0 or y == 0 or x == ancho - 1 or y == alto - 1:
-				matriz[x][y] = lleno
-			else:
-				vaciar = dado.randi_range(0, 100)
-				matriz[x][y] = vacio if vaciar > densidad else lleno
-
 func tirar_dado() -> void:
 	if usar_semilla:
 		if semilla.is_valid_int():
@@ -110,3 +137,5 @@ func tirar_dado() -> void:
 			dado.seed = hash(semilla)
 	else:
 		dado.randomize()
+
+#endregion - Funciones subordinadas y de apoyo
